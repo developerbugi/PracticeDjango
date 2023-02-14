b8aeb04b752e7b40ea07d0a5c1c694d8ffc51cc3
거부길 <sopens@naver.com>
2023-02-14 20:18:32 +0900

    DB를 사용하여 피드 불러오기

--- CloneInstagram/content/views.py
+++ CloneInstagram/content/views.py
@@ -6,14 +6,14 @@ class Main(APIView):
     def get(self, request):
         print("GET 형식 호출")
         # select * from content_feed 와 동일
-        fead_list = Feed.objects.all()
+        feed_list = Feed.objects.all()
 
         # DB 넘어온 값 출력
-        for feed in fead_list:
+        for feed in feed_list:
             print(feed.content)
             print(feed.image)
             print(feed.profile_image)
             print(feed.user_id)
             print(feed.like_count)
 
-        return render(request, "cloneinstagram/main.html")
+        return render(request, "cloneinstagram/main.html", context=dict(feeds=feed_list))
